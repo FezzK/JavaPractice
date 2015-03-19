7a556a34c4cd6ec0cb7e25aae13171bc85101fa9
강지헌 <wlgjs0458@naver.com>
2015-03-19 11:17:26 +0900

    첫 번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/basic/JavaProject.java
@@ -0,0 +1,20 @@
+package org.dimigo.basic;
+
+public class JavaProject {
+
+	public static void main(String[] args) {
+		String a="아이유";
+		boolean b=true;
+		int c=23;
+		double d=161.8;
+		float e=44.3f;
+		char f='A';
+		System.out.println("<<아이유 프로필>>");
+		System.out.println("이름 : "+a);
+		System.out.println("성별 : "+((b)?"여자":"남자"));
+		System.out.println("나이 : "+c+" 세");
+		System.out.println("키 : "+d+" cm");
+		System.out.println("몸무게 : "+e+" kg");
+		System.out.println("혈액형 : "+f+" 형");
+	}
+}
