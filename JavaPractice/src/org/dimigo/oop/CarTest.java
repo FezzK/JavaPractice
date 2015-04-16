693d349fff83b1336adbe7cbd341d77bd90d8847
Kang <Kang@adminstrator>
2015-04-16 13:44:20 +0900

    과제제출

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -39,7 +39,7 @@ public class CarTest {
 		System.out.println("색상 : " + car.getColor());
 		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
 		System.out.println("가격 : " + String.format("%,d", car.getPrice()) + "원");*/
-		Car2 car=new Car2("현대자동차","제네시스","검정색",225,50000000);
+		Car3 car=new Car3("현대자동차","제네시스","검정색",225,50000000);
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : " + car.getModel());
 		System.out.println("색상 : " + car.getColor());
@@ -47,7 +47,7 @@ public class CarTest {
 		System.out.println("가격 : " + String.format("%,d", car.getPrice()) + "원");
 		System.out.println("");
 		System.out.println("");
-		car=new Car2("기아자동차","K7","흰색",246,40000000);
+		car=new Car3("기아자동차","K7","흰색",246);
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : " + car.getModel());
 		System.out.println("색상 : " + car.getColor());
@@ -55,7 +55,7 @@ public class CarTest {
 		System.out.println("가격 : " + String.format("%,d", car.getPrice()) + "원");
 		System.out.println("");
 		System.out.println("");
-		car=new Car2("삼성자동차","SM7","회색",200,38000000);
+		car=new Car3("삼성자동차","SM7","회색");
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : " + car.getModel());
 		System.out.println("색상 : " + car.getColor());
