1b64cb07fc075c9005af80523b8ea8f300ab8ce4
Kang <Kang@adminstrator>
2015-05-18 11:23:00 +0900

    과제제출

--- JavaPractice/src/org/dimigo/oop/Score.java
+++ JavaPractice/src/org/dimigo/oop/Score.java
@@ -6,29 +6,25 @@ public class Score
 	{
 		Scanner scanner = new Scanner(System.in);
 		System.out.printf("국어 점수 입력 =>");
-		int kor = scanner.nextInt();
+		int kor=scanner.nextInt();
 		System.out.printf("수학 점수 입력 =>");
-		int mat = scanner.nextInt();
+		int mat=scanner.nextInt();
 		System.out.printf("영어 점수 입력 =>");
-		int eng = scanner.nextInt();
-		int sum = kor+mat+eng;
-		double avg = (kor+mat+eng)/3.0;
+		int eng=scanner.nextInt();
+		int sum=kor+mat+eng;
+		double avg=(kor+mat+eng)/3.0;
 		System.out.println();
 		StringBuilder sb = new StringBuilder()
 		  	.append("<<점수출력>>\n")
 		    .append("국어점수 : ")
 		    .append(kor)
-		    .append("점\n")
-		    .append("수학점수 : ")
+		    .append("점\n수학점수 : ")
 		    .append(mat)
-		    .append("점\n")
-		    .append("영어점수 : ")
+		    .append("점\n영어점수 : ")
 		    .append(eng)
-		    .append("점\n")
-		    .append("총점 : ")
+		    .append("점\n총점 : ")
 		    .append(sum)
-		    .append("점\n")
-	    	.append("평균 : ")
+		    .append("점\n평균 : ")
 	    	.append(String.format("%.1f", avg))
 	    	.append("점\n");
 		System.out.println(sb);
